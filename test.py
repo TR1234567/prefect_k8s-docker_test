2ada046e5a0e22660e491c63311b26b82a44ea20
TR1234567 <raviel_007@hotmail.com>
2022-06-21 16:01:14 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,26 @@
-from prefect import task
-from prefect.engine import TaskRunner
-
+import prefect
+from prefect import task, Flow
 
 @task
-def number_task():
+def add_one(x):
+    """Operates one number at a time"""
+    return x + 1
+
+@task(name="sum")
+def sum_numbers(y):
+    """Operates on an iterable of numbers"""
+    result = sum(y)
     logger = prefect.context["logger"]
-    logger.info("Hello!")
-    return 42
+    logger.info("The sum is {}".format(result))
+    return result
 
 
-with  Flow("hello-flow") as flow 
-    f.add_task(number_task)
+## Imperative API
+flow = Flow("Map Reduce")
+flow.set_dependencies(add_one, keyword_tasks={"x": [1, 2, 3, 4]}, mapped=True)
+flow.set_dependencies(sum_numbers, keyword_tasks={"y": add_one})
+
+## Functional API
+with Flow("Map Reduce") as flow:
+    mapped_result = add_one.map(x=[1, 2, 3, 4])
+    summed_result = sum_numbers(mapped_result)
