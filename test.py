c94e7b3c35484f47a45202cc34f620d0cf6b4a92
TR1234567 <raviel_007@hotmail.com>
2022-06-21 13:48:46 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print("hello2")
+
+import prefect
+from prefect import task, Flow
+
+@task
+def plus_one(x):
+    logger = prefect.context.get("logger")
+    logger.info("test1")
+    return x + 1
+
+with Flow("flow") as flow:
+    # image="test"))
+    plus_one(1)
