cd777032815fefa66a9a163e424bf525d861afa1
TR1234567 <raviel_007@hotmail.com>
2022-06-21 16:06:52 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,24 +2,11 @@ import prefect
 from prefect import task, Flow
 
 @task
-def add_one(x):
-    """Operates one number at a time"""
-    return x + 1
+def hello_task():
+    logger = prefect.context.get("logger")
+    logger.info("Hello world!")
 
-@task(name="sum")
-def sum_numbers(y):
-    """Operates on an iterable of numbers"""
-    result = sum(y)
-    logger = prefect.context["logger"]
-    logger.info("The sum is {}".format(result))
-    return result
-
-
-
-## Functional API
 with Flow("hello-flow") as flow:
-    mapped_result = add_one.map(x=[1, 2, 3, 4])
-    summed_result = sum_numbers(mapped_result)
-
+    hello_task()
 
 flow.run()
