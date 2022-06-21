8d5d2b5998190b574699ec456e5c82b5cc2d7b0e
TR1234567 <raviel_007@hotmail.com>
2022-06-21 15:44:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,7 @@ import prefect
 from prefect import task, Flow
 
 @task
-def show():
-    logger = prefect.context.get("logger")
-    logger.info("flow run ")
+my_shell = ShellTask(shell="bash")
 
 with Flow("hello-flow") as flow:
-    # image="test"))
-    show()
+    task1 = my_shell(command="ls")
